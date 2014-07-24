4531a440f6864f7410fef9557d068ff49a58c8bc
kr8tivitco <knmckinney@Kristons-MacBook-Pro.local>
2014-07-23 20:14:15 -0400

    4th commit
    Added GameOver function
    Added Summary Overlay with score and re-start game

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
     
     while(arr.length < 5){
       
-      var randomnumber=Math.ceil(Math.random()*(10 - arr.length)), i;
+      var randomnumber=Math.ceil(Math.random()*(16 - arr.length)), i;
     
         for(i=0; i < arr.length; i++) {
     
@@ -107,6 +107,48 @@ $(document).ready(function() {
         qNum : 10,
         answ : 2,
         fact: "Carmelo Anthony scored 20 points and gathered 10 rebounds in the finals game"
+       },
+       {
+        ques: "Which year did Kobe win a gold medal at the Olympics as a member of the USA national team?",
+        opt: ["2006", "2008", "2004", "2002"],
+        qNum : 11,
+        answ : 1,
+        fact: "The USA defeated Spain 118-107, it ended an 8year drought at major international competition"
+       },
+       {
+        ques: "Which player was the first to win back-to-back NBA Finals MVP awards?",
+        opt: ["Michael Jordan", "Isaiah Thomas", "Wilt Chamberlin", "Larry Bird"],
+        qNum : 12,
+        answ : 0,
+        fact: "Michael Jordan won three MVP Awards in a row from 1991-93."
+       },
+       {
+        ques: "Which number was worn by point guard Steve Nash when he was drafted in 1996 by the Phoenix Suns?",
+        opt: ["#15", "#11", "#9", "#13"],
+        qNum : 13,
+        answ : 3,
+        fact: "Steve Nash, wearing #13, is a 2x MVP, who has played for 3 NBA teams"
+       },
+       {
+        ques: "In what year was the first NBA Draft Lottery held?",
+        opt: ["1978", "1980", "1975", "1985"],
+        qNum : 14,
+        answ : 3,
+        fact: "The first NBA Draft lottery was held in 1985. Prior to this, the worst teams would flip a coin to see who would get the top pick."
+       },
+       {
+        ques: "Which team drafted Shaquille O'Neal?",
+        opt: ["Miami Heat", "Los Angeles Lakers", "Orlando Magic", "Phoenix Suns"],
+        qNum : 15,
+        answ : 2,
+        fact: "The Orlando Magic drafted O'Neal in 1992"
+       },
+       {
+        ques: "1Who was the first high school player to be taken #1 overall in the NBA Draft?",
+        opt: ["Kevin Garnett", "Kwame Brown", "LeBron James", "Kobe Bryant"],
+        qNum : 16,
+        answ : 1,
+        fact: "The Washington Wizards took Brown #1 in 2001."
        }];
 /////////////////////////////////////////////////////////////////////////////
   
@@ -168,7 +210,7 @@ $(document).ready(function() {
               $("#fact").html(questions[askNum].fact);
                 playerAnsw[k] = $('input[type="radio"]:checked').val();
               //alert("Radion Button Value = "+ $('input[name="radioBtn[]"]:checked').val() +" Anwers to Question=  "+ questions[askNum].answ + "   Array PlayerAnswer = " + playerAnsw + "   Correct # = " + correctNum + "   Incorrect # = " + incorrectNum);
-              $("#correct").html(incorrectNum + " OF "); 
+              //$("#correct").html(incorrectNum + " OF "); 
               
             }
               $("#warning").html("");   
@@ -197,6 +239,8 @@ $(document).ready(function() {
 
               $('input[type="radio"]').attr('checked', false);
               $("#warning").html("");
+              $("#fact").html("");
+
 
               if (j <5 ) {
                 playGame();
@@ -210,11 +254,22 @@ $(document).ready(function() {
         function gameOver () {
 
           $(".overlay").fadeIn(1000);
+          var percScore = ((correctNum/j)*100);
+          $(".score").html("You Answered " +correctNum+ " Out of " +j+ " Correctly. For A Score Of " +percScore+ "%");
 
 
           /*--- Hide information modal box ---*/
           $("a.ovrClose").click(function(){
             $(".overlay").fadeOut(1000);
+
+            randNumFunc();
+
+            j=0;
+            correctNum=0;
+            incorrectNum=0;
+
+                        playGame();
+
           });
         }
 
